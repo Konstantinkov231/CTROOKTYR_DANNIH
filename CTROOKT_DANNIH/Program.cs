dbbee260542872728fed9dc6a2a3ec6baae27858
Konstantinkov231 <139924536+Konstantinkov231@users.noreply.github.com>
2026-03-26 00:26:55 +0300

    ff

---
+++ CTROOKT_DANNIH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int n;
+
+System.Console.WriteLine("Введите натуральное целое число >>>");
+n = Convert.ToInt32(Console.ReadLine());
+
+int sm = n;
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                sm = i;
+                break;
+            }
+        }
+
+        int a = n / sm;
+        int b = n - a;
+
+        Console.WriteLine($"Выполнил: Ковачёв Константин Владимирович;\n Группа: 020303-АИСа-о25;\n\n Результат: {a} {b}");
